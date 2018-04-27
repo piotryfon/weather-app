711b4f38aa8c5c7061ee286a920a30a8883c347e
piotryfon <pit.pit@wp.pl>
2018-04-27 17:13:06 +0200

    fetch weather

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends React.Component {
     
     getWeather = async (e) => {
         e.preventDefault();
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?id=756135&appid=${API_KEY}&units=metric`);
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     
         const data = await api_call.json();
 
