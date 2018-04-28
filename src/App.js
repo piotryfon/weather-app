ee043522c4b1c0727a5695659e1d9323b4d12a91
piotryfon <pit.pit@wp.pl>
2018-04-28 22:22:57 +0200

    display props error

--- src/App.js
+++ src/App.js
@@ -35,6 +35,15 @@ class App extends React.Component {
             description: data.weather[0].description,
             error: "",
         });
+        } else {
+            this.setState({
+                temperature: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                error: "Please enter the value.",
+            });
         }
     }
     render() {
