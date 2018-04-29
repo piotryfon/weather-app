04fa047d9dff89f6eedc84c6f155867c051e4218
piotryfon <pit.pit@wp.pl>
2018-04-29 18:58:15 +0200

    class to const in Weather, Titles, Form

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-class Weather extends React.Component {
-  
-    render() {
-        return(
-            <div>
-                {this.props.city && this.props.country && <p>Location: {this.props.city}  {this.props.country}</p>}
-                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
-                {this.props.description && <p>Description: {this.props.description}</p>}
-                {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
-                {this.props.error && <p>{this.props.error}</p>}
+const Weather = props => (
+        <div>
+            
+            {props.city && props.country && <p>Location: {props.city}  {props.country}</p>}
+            {props.temperature && <p>Temperature: {props.temperature}</p>}
+            {props.description && <p>Description: {props.description}</p>}
+            {props.humidity && <p>Humidity: {props.humidity}</p>}
+            {props.error && <p>{props.error}</p>}
                 
-            </div>
-        )
-    }
-}
+        </div>
+) ;
+  
+   
 export default Weather;
