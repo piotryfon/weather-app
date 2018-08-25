e92537b57c40ecd8da2d9805b83564679662429f
piotryfon <pit.pit@wp.pl>
2018-08-25 18:41:49 +0200

    zamiana http na https i style do input

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ class App extends React.Component {
         e.preventDefault();
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
-        const api_call_1 = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=${API_KEY}&units=metric`)
+        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+        const api_call_1 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=${API_KEY}&units=metric`)
         const data = await api_call.json();
         const data1 = await api_call_1.json();
         if(city && country) {
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Form = props => (
 
     <form onSubmit={props.getWeather}>
-        <input type="text" name="city" placeholder="City..."/>
-        <input type="text" name="country" placeholder="Country..."/>
-        <button>Get Weather</button>
+        <input className="input" type="text" name="city" placeholder="City..."/>
+        <input  className="input"type="text" name="country" placeholder="Country..."/>
+        <button className="input btn">Get Weather</button>
     </form>
 );
 export default Form;
