5ec940fde949f08d943671d81202f198ac2981a9
piotryfon <pit.pit@wp.pl>
2018-09-03 18:28:52 +0200

    new background

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Titles = () => (
     <div>
         <h1>Weather Finder</h1>
-        <p>Find out temperature, conditions and more...</p>
+        <p>Find out temperature and conditions.</p>
     </div>
 );
 export default Titles;
